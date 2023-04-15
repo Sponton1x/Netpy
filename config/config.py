cd5658cfd2448f0af0213843ea713cdf0508344f
Sponton1x <kubawrzeszcz0@gmail.com>
2023-04-15 20:35:14 +0200

    Code refactor

--- config/config.py
+++ config/config.py
@@ -1,35 +1,41 @@
-#! usr/bin/python3
+#!/usr/bin/python3
 # config.py
 
 import time
 import sys
 import os
 
+
+current_year = int(time.strftime("%Y"))
+
+
 class NetpyConfig:
-    VERSION = '1.0.0'
-    DESCRIPTION = "The Network Python Package Tools"
-    AUTHOR = "Sponton1x"
-    WEBSITE = "https://xyz.sponton1x.repl.co"
-    COPYRIGHT = "(c) 2023 Sponton1x"
-    LICENSE = "CC BY-SA 4.0"
+    __version__ = '1.0.0'
+    __description__ = "The Network Python Package Tools"
+    __author__ = "Sponton1x"
+    __website__ = "https://xyz.sponton1x.repl.co"
+    __copyright__ = f"(c) {current_year} Sponton1x"
+    __license__ = "CC BY-SA 4.0"
+    __module_dir__ = "modules"
+
+    def load_animation(self):
+        lowerstr = f'starting the netpy tool >> version >> {config.__version__}'
+        upperstr = lowerstr.upper()
+        for x in range(len(lowerstr)):
+            s = '\r' + lowerstr[0:x] + upperstr[x] + lowerstr[x+1:] + '\r'
+            sys.stdout.write(s)
+            sys.stdout.flush()
+            time.sleep(0.1)
+
+    def import_modules(self):
+        for filename in os.listdir(config.__module_dir__):
+            if filename.endswith(".py"):
+                module_name = filename[:-3]
+                try:
+                    __import__(f"{config.__module_dir__}.{module_name}")
+                except Exception as e:
+                    print(f"Error during loading modules >> {e}")
+                    sys.exit()
 
 
-def load_animation():
-    lowerstr = 'starting the netpy tool...'
-    upperstr = lowerstr.upper()
-    for x in range(len(lowerstr)):
-         s = '\r' + lowerstr[0:x] + upperstr[x] + lowerstr[x+1:] + '\r'
-         sys.stdout.write(s)
-         sys.stdout.flush()
-         time.sleep(0.1)
-
-def import_modules():
-    module_dir = "modules"
-    for filename in os.listdir(module_dir):
-        if filename.endswith(".py"):
-            module_name = filename[:-3]
-            try:
-                __import__(f"{module_dir}.{module_name}")
-            except Exception as e:
-                print(f"Error during loading modules >> {e}")
-                sys.exit()
+config = NetpyConfig()
--- main.py
+++ main.py
@@ -2,18 +2,18 @@
 # usr/bin/python3
 
 import argparse, sys
-from config.config import load_animation, import_modules
-from config.args import create_parser
+from config.config import config
+from config.args import ParserObjClass
 
 try:
-    import_modules()
+    config.import_modules()
 except Exception as e:
     print(f"Error while excuting function >> {e}")
     sys.exit()
 
 
 if __name__ == '__main__':
-    load_animation()
-    parser = create_parser()
+    config.load_animation()
+    parser = ParserObjClass.create_parser()
     args = parser.parse_args()
     args.func(args)
